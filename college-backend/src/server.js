0f6ff40712f2089587fbc2a6b79375aebaa6ff41
krish3239 <krishbhardwaj3239@gmail.com>
2025-09-13 20:49:55 +0530

    the chnage

--- college-backend/src/server.js
+++ college-backend/src/server.js
@@ -1,8 +1,4 @@
-import dotenv from "dotenv"
+
 import app from "./app.js";
 
-dotenv.config()
-const PORT = process.env.PORT || 5000;
-
-
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+export default app;
