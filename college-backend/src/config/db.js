f3120664c27dcfa0e680290dadda24c873a0f1cb
krishfixerra <krish.bhardwaj@fixerra.in>
2025-11-03 00:23:25 +0530

    the changes

--- college-backend/src/config/db.js
+++ college-backend/src/config/db.js
@@ -5,8 +5,6 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
-       const indexes = await Student.collection.indexes();
-    console.log("Indexes on students collection:", indexes);
   } catch (err) {
     console.error("❌ DB connection failed:", err.message);
     process.exit(1);
